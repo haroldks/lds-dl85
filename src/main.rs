ca2dac4cd2c655ad004471591708083ed027c50c
HKsilver <kiossouharold@gmail.com>
2021-09-20 15:50:44 +0200

    Add Mining operations on itemsets.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use crate::data::*;
-
 mod mining;
 mod data;
 
-
+use crate::data::*;
+use crate::mining::*;
 use bit_vec::BitVec;
-
+use crate::mining::ItemsetOps;
+use std::thread::current;
 
 
 fn main() {
 
     let mut data = Data::new("datasets/pendigits.txt".to_string()).unwrap();
 
-    println!("{:?}", data.nclasses);
+    let items: Vec<Item> = vec![(12, true), (1, true)];
+
+    let mut its_ops = ItemsetOps::new(items, &data, None, None, None, data.ntransactions, false);
+
+
+
+    // println!("{:?}",its_ops.freq());
+    println!("{:?}",its_ops.freq());
+    println!("{:?}", its_ops.union_cover(&(11, true)));
+    println!("{:?}", its_ops.union_cover(&(19, true)));
+     println!("{:?}", data.data[19]);
+    // println!("{:?}", its_ops.current);
 
 
 
--- src/mining.rs
+++ src/mining.rs
@@ -1,46 +1,90 @@
 use bit_vec::BitVec;
+use super::data::Data;
 
-pub type Transactions = Vec<BitVec>;
-pub type Attribute = u32;
+pub type Attribute = usize;
 pub type Item = (Attribute, bool);
 //pub type Itemset = Vec<Item>; // I can use a structure to have all data stored in it quickly like support, freq and so one when each function is called
 
 
-pub struct Itemset{
+pub struct ItemsetOps<'a>{
 
-    its: Vec<Item>,
-    support: Option<f32>,
-    frequency: Option<f32>
+    pub current: Vec<Item>,
+    data: &'a Data,
+    support: Option<usize>,
+    frequency: Option<f32>,
+    mask: Option<BitVec>,
+    ntransactions: usize,
+    updated: bool
 
 }
 
-impl Itemset{
+impl <'a> ItemsetOps <'a> {
 
-    // pub fn new(its: Vec<Item>, support: Option<f32>, frequency: Option<f32>) -> Itemset {
-    //     Itemset(its, support, frequency)
-    // }
-
-    pub fn cover(&self){
-        unimplemented!("Return a view of all the transactions associated with the actual itemset.");
+    pub fn new(current: Vec<Item>, data: &Data, support: Option<usize>, frequency: Option<f32>, mask: Option<BitVec>, ntransactions:usize, updated:bool) -> ItemsetOps {
+        ItemsetOps{current, data,  support, frequency, mask, ntransactions, updated}
     }
 
-    pub fn union_cover(&self, second_its: &Itemset, transactions: &Transactions){
-        unimplemented!("Union of two itemsets. Should return the cover of the new Itemset. All the\
-        transactions associatied to the new itemset");
+    pub fn union(&mut self, second_its: &Item){
+        self.current.push(*second_its);
+        self.updated = false;
+        self.update_mask(&second_its);
+        self.support();
     }
 
-    pub fn support(&self){
+    pub fn union_cover(&mut self, second_its: &Item) -> usize {
+        self.current.push(*second_its);
+        self.updated = false;
+        self.update_mask(&second_its);
+        self.support()
+    }
 
-        unimplemented!("Return the support of the actual itemset.");
+    fn update_mask(&mut self, item: &Item){
+          let mut  mask = self.mask.as_mut().unwrap();
+            let mut item_vec = self.data.data[item.0].clone();
+            if !item.1 {
+                item_vec.negate();
+            }
+            mask.and(&item_vec);
+            self.updated = false;
+    }
+
+    fn  compute_support_from_mask(&mut self) -> usize {
+        if !self.mask.is_some(){
+            self.mask = Option::from(BitVec::from_elem(self.ntransactions, true));
+        }
+        for mut item in &self.current.clone(){
+           self.update_mask(&item);
+        }
+        let mut mask = self.mask.as_mut().unwrap();
+        self.support = Option::from(mask.iter().filter(|x| *x).count());
+        self.frequency = Option::from(self.support.unwrap() as f32 / self.ntransactions as f32);
+        self.updated = true;
+        self.support.unwrap()
+    }
+
+
+    pub fn support(&mut self) -> usize {
+        return if self.support.is_some() && self.updated{
+            self.support.unwrap()
+        } else if self.mask.is_some() {
+            let mask = self.mask.as_ref().unwrap();
+            self.support = Option::from(mask.iter().filter(|x| *x).count());
+            self.frequency = Option::from(self.support.unwrap() as f32 / self.ntransactions as f32);
+            self.updated = true;
+            self.support.unwrap()
+        } else {
+            self.mask = Option::from(BitVec::from_elem(self.ntransactions, true));
+            self.compute_support_from_mask()
+        }
 
     }
 
-    pub fn freq(&self){
-        unimplemented!("Return the freq of the actual itemset.");
-    }
 
-    pub fn compute_infos(&self,  transactions: &Transactions){
-        unimplemented!("Compute all for an itemset on a set of transactions.")
+    pub fn freq(&mut self) -> f32 {
+        if !self.updated{
+            self.support();
+        }
+        self.frequency.unwrap()
     }
 
 }
