20e3c4c5091b4ab6af1f0683931d4a160fede0ea
HKsilver <kiossouharold@gmail.com>
2021-10-11 15:14:17 +0200

    Update Prediction methods in solution.rs

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use crate::cache::trie::*;
 use crate::data::dt_chuncked::*;
 use crate::dl85::basic_dl85::DL85;
 use crate::mining::its_ops_chunked::ItemsetOpsChunked;
-use crate::solution::solution::get_solution_tree;
-
+use crate::solution::solution::{compute_metrics, get_data_as_transactions_and_target, get_solution_tree, predict};
+use crate::tree::Tree;
 
 mod mining;
 mod data;
@@ -15,10 +15,23 @@ mod solution;
 
 fn main() { // TODO: Unit tests
 
-    let datac = DataChuncked::new("datasets/tic-tac-toe.txt".to_string()).unwrap();
+    let filename = "datasets/tic-tac-toe.txt".to_string();
+
+    let datac = DataChuncked::new(filename.clone()).unwrap();
     let its_opsd = ItemsetOpsChunked::new(&datac, None, None, datac.ntransactions, false, datac.data[0].len());
-    let mut algo = DL85::new(100, 5, 500., 2., Trie::new(), its_opsd);
+    let mut algo = DL85::new(30, 4, 1000., 0., Trie::new(), its_opsd);
     let output = algo.run();
     let data = get_solution_tree(output.0);
-    println!("{:?}", data.0);
+    let dd = get_data_as_transactions_and_target(filename.clone()).unwrap();
+    println!("Tree: {:?}", data.0);
+    let y_pred = predict(dd.0.clone(), data.0.clone());
+    let mut a = vec![];
+    for i in 0..y_pred.len() {
+        a.push((y_pred[i] == dd.1[i]));
+    }
+    println!("Accuracy: {:?}", a.iter().filter(|x| **x).count() as f32 / dd.1.len() as f32);
+    println!("{:?}", y_pred);
+    println!("Confusion Matrix: {:?}", compute_metrics(dd.1, y_pred, 2));
 }
+
+
--- src/solution/solution.rs
+++ src/solution/solution.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error};
+use bit_vec::BitVec;
 use crate::cache::trie::Trie;
 use crate::mining::types_def::Item;
 use crate::tree::Tree;
@@ -22,6 +25,7 @@ pub fn get_solution_tree(cache: Trie) -> (Tree, Trie) {
 
 fn get_sub_tree(parent: Vec<Item>, mut cache: Trie) -> (Trie, Tree) {
     let parent_node = cache.get(&parent).unwrap();
+
     let len = parent.len();
 
     let parent_attribute = parent[len - 1].0;
@@ -34,18 +38,76 @@ fn get_sub_tree(parent: Vec<Item>, mut cache: Trie) -> (Trie, Tree) {
         (cache, final_tree)
     } else {
         let mut item_set_vec = parent.clone();
-        item_set_vec.push((parent_node_data.test, false)); //left
-        item_set_vec.sort_unstable();
+        let checker = item_set_vec.iter().filter(|it| it.0 == parent_node_data.test).collect::<Vec<&Item>>();
+        if checker.len() > 0 {
+            final_tree.is_leaf = true;
+            final_tree.max_class = parent_node_data.max_class;
+            final_tree.error = Option::from(parent_node_data.node_error);
+            (cache, final_tree)
+        } else {
+            item_set_vec.push((parent_node_data.test, false)); //left
+            item_set_vec.sort_unstable();
 
-        let data = get_sub_tree(item_set_vec, cache);
-        final_tree.left.push(data.1);
+            let data = get_sub_tree(item_set_vec, cache);
+            final_tree.left.push(data.1);
 
-        let mut item_set_vec = parent.clone();
-        item_set_vec.push((parent_node_data.test, true)); //right
-        item_set_vec.sort_unstable();
+            let mut item_set_vec = parent.clone();
+            item_set_vec.push((parent_node_data.test, true)); //right
+            item_set_vec.sort_unstable();
 
-        let data = get_sub_tree(item_set_vec, data.0);
-        final_tree.right.push(data.1);
-        (data.0, final_tree)
+
+            let data = get_sub_tree(item_set_vec, data.0);
+            final_tree.right.push(data.1);
+            (data.0, final_tree)
+        }
     }
+}
+
+
+pub fn predict(transactions: Vec<BitVec>, mut tree: Tree) -> Vec<usize> {
+    let mut y_pred: Vec<usize> = Vec::new();
+    for transaction in transactions {
+        let mut clone_tree = tree.clone();
+        while !clone_tree.is_leaf {
+            if !transaction[clone_tree.root] {
+                clone_tree = clone_tree.left[0].clone();
+            } else {
+                clone_tree = clone_tree.right[0].clone();
+            }
+        }
+        y_pred.push(clone_tree.max_class);
+    }
+    return y_pred;
+}
+
+pub fn get_data_as_transactions_and_target(filename: String) -> Result<(Vec<BitVec>, Vec<usize>), Error> {
+    let input = File::open(&filename)?; //Error Handling for missing filename
+    let buffered = BufReader::new(input); // Buffer for the file
+    let data_lines: Vec<String> = buffered.lines().map(|x| x.unwrap()).collect();
+    let nattributes = data_lines[0].split_ascii_whitespace().collect::<Vec<&str>>().len() - 1;
+    let ntransactions = data_lines.len();
+    let mut inputs = vec![BitVec::from_elem(nattributes, false); ntransactions];
+    let mut target = vec![];
+    for (i, line) in data_lines.iter().enumerate() {
+        let line = line.split_ascii_whitespace().collect::<Vec<&str>>();
+        for (j, l) in line.iter().enumerate() {
+            match j {
+                0 => { target.push(l.parse::<usize>().unwrap()) }
+                _ => {
+                    inputs[i].set((j - 1), l == &"1")
+                }
+            }
+        }
+    }
+    return Ok((inputs, target));
+}
+
+
+pub fn compute_metrics(y_test: Vec<usize>, y_pred: Vec<usize>, nclasses: usize) -> Vec<Vec<i32>> {
+    let mut matrix = vec![vec![0; nclasses]; nclasses];
+    let len = y_test.len();
+    for i in 0..len {
+        matrix[y_test[i]][y_pred[i]] += 1;
+    }
+    return matrix;
 }
