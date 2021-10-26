d80c877664ce4a1d200c9cc898fd587ca0247b54
HKsilver <kiossouharold@gmail.com>
2021-10-26 21:55:36 +0200

    Add discrepancy parameter

--- src/node/node.rs
+++ src/node/node.rs
@@ -10,6 +10,7 @@ pub struct Node {
     // FIXME: Useless I think
     pub test: Attribute,
     pub leaf_error: f64,
+    pub current_discrepancy: Option<u64>,
     pub node_error: f64,
     pub lower_bound: f64,
     pub max_class: usize,
@@ -25,6 +26,7 @@ impl Node {
             is_explored: false,
             current_depth,
             test,
+            current_discrepancy: None,
             leaf_error: <f64>::MAX,
             node_error: <f64>::MAX,
             lower_bound: 0f64,
@@ -36,6 +38,6 @@ impl Node {
 
 impl fmt::Display for Node {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        writeln!(f, "[current_depth : {}, is_leaf  :  {}, is_explored  :  {},   test:  {},  leaf_error:  {},  node_error:  {}, max class : {}]", self.current_depth, self.is_leaf, self.is_explored, self.test, self.leaf_error, self.node_error, self.max_class)
+        writeln!(f, "[current_depth : {}, is_leaf  :  {}, is_explored  :  {},   test:  {},  leaf_error:  {},  node_error:  {}, max class : {}, current_discrepancy  :  {:?}]", self.current_depth, self.is_leaf, self.is_explored, self.test, self.leaf_error, self.node_error, self.max_class, self.current_discrepancy)
     }
 }
