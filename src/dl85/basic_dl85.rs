d74263ae8c155e2162240098e6f6e4ce2b530c1c
HKsilver <kiossouharold@gmail.com>
2021-12-11 13:48:04 +0100

    Updates

--- src/dl85/basic_dl85.rs
+++ src/dl85/basic_dl85.rs
@@ -90,6 +90,7 @@ impl<'a> DL85 {
     pub fn run<T: ItemsetBitvector>(&mut self, min_support: u64, max_depth: u64, max_error: f64, time_limit: f64, error_save_time: i32, use_info_gain: bool, use_discrepancy: bool, reload_cache: bool, mut its_ops: T, mut cache: Trie) -> (Trie, T, Node, Instant) {
         let init_distribution = its_ops.classes_cover();
         println!("Train distribution: {:?}", init_distribution);
+        println!("Number of itemsets: {:?}", its_ops.get_infos().1*2);
 
         let mut scheduler = Scheduler::new(); // Scheduler for the error save time
 
@@ -196,7 +197,7 @@ impl<'a> DL85 {
 
     fn recursion<T: ItemsetBitvector>(mut cache: Trie, mut its_op: T, current_itemset: Vec<Item>, last_attribute: Attribute, next_candidates: Vec<Attribute>, upper_bound: f64, depth: u64, max_depth: u64, use_discrepancy: bool, current_discrepancy: Option<u64>, mut max_discrepancy: Option<u64>, min_support: u64, max_error: f64, mut parent_node_data: Node, instant: Instant, time_limit: f64, use_info_gain: bool, reload_cache: bool, original_len: usize) -> (Trie, T, Node, Instant) {
         unsafe {
-            print!("ITER : {} % \r", ITERS as f64 / cache.max_iterations as f64);
+            print!("ITER : {} % \r", ITERS as f64 * 100. / cache.max_iterations as f64);
             CURRENT_ERROR = cache.root.data.node_error;
         }
 
