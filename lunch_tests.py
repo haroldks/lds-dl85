a04ca4000ee05e5bd443fbb17d61ee368ecc7366
HKsilver <kiossouharold@gmail.com>
2022-03-11 18:26:59 +0100

    A little polish in tests

--- lunch_tests.py
+++ lunch_tests.py
@@ -1,63 +1,36 @@
-import os
 import shutil
-import json
-import subprocess
 from multiprocessing import Pool
+from functools import partial
+
+from utils import *
 
 # Directories constants
 DATASET_DIR = "datasets"
 CONFIG_BASE_DIR = "confs_dir"
 RESULTS_DIR = "CONFS_RESULTS"
 
-
 # TEST PARAMETERS
-ALLOW_DISCREPANCIES = [True, False]
-USE_INFORMATION_GAIN = [True, False]
-TIMEOUTS = [30, 60, 90]
+ALLOW_DISCREPANCIES = [True]
+USE_INFORMATION_GAIN = [True]
+TIMEOUTS = [10]
 PARAMETERS = {
-    "support": 1,
-    "depth": 9,
+    "support": 100,
+    "depth": 3,
 }
 
 # Execution Information
 BIN_FILE = "target/release/sandbox"
 N_THREADS = 8
 
-
 if os.path.exists(RESULTS_DIR):
     shutil.rmtree(RESULTS_DIR)
+
+if os.path.exists(CONFIG_BASE_DIR):
+    shutil.rmtree(CONFIG_BASE_DIR)
+
+
 os.makedirs(RESULTS_DIR)
-
-
-def generate_config_files(datasets_dir, params, out_dir):  # TODO: Allows Multiple timeout ?
-    files = os.listdir(datasets_dir)
-
-    for file in files:
-        file_path = os.path.join(datasets_dir, file)
-        file_data = params
-        file_data['input'] = file_path
-        basename = os.path.splitext(os.path.basename(file_path))[0]
-        basename = f"tree_{basename}_supp_{params['support']}_depth_" \
-                   f"{params['depth']}_timeout_{params['timeout']}.json"
-        file_data["output"] = f"{RESULTS_DIR}/{out_dir.split('/')[-1]}/{basename}"
-        with open(f"{out_dir}/{basename}", "w") as f:
-            json.dump(file_data, f)
-
-
-def get_conf_files(base_dir):
-    files_path = list()
-    for file in os.listdir(base_dir):
-        path = os.path.join(base_dir, file)
-        if os.path.isdir(path):
-            files_path += get_conf_files(path)
-        else:
-            files_path.append(path)
-    return files_path
-
-
-def run_test(config):
-    subprocess.run([BIN_FILE, '-c', config])
-
+os.makedirs(CONFIG_BASE_DIR)
 
 parameters = list()
 output_dir = list()
@@ -83,22 +56,16 @@ for allow_dis in ALLOW_DISCREPANCIES:
 conf_tuples = list(zip(parameters, output_dir))
 
 for c in conf_tuples:
-    generate_config_files(DATASET_DIR, *c)
+    generate_config_files(DATASET_DIR, *c, RESULTS_DIR)
 
 # Run Tests
-
-conf_files = get_conf_files(CONFIG_BASE_DIR)
+conf_files = get_files(CONFIG_BASE_DIR)
 
 with Pool(N_THREADS) as p:
-    p.map(run_test, conf_files)
+    p.map(partial(run_test, BIN_FILE), conf_files)
 
-#
-#
-# def launch_rust(config):
-#     subprocess.run(['./target/release/sandbox', '-c', config])
-#
-#
-# # Merge Results
-#
-#
+# Merge Results
+
+df = get_results_as_csv(RESULTS_DIR, save=f"{RESULTS_DIR}/results.csv")
+
 # # Remove Outputs
---
+++ utils.py
@@ -0,0 +1,53 @@
+import os
+import json
+import subprocess
+import pandas as pd
+
+
+def generate_config_files(datasets_dir, params, out_dir, results_dir):  # TODO: Allows Multiple timeout ?
+    files = os.listdir(datasets_dir)
+
+    for file in files:
+        file_path = os.path.join(datasets_dir, file)
+        file_data = params
+        file_data['input'] = file_path
+        basename = os.path.splitext(os.path.basename(file_path))[0]
+        basename = f"tree_{basename}_supp_{params['support']}_depth_" \
+                   f"{params['depth']}_timeout_{params['timeout']}.json"
+        file_data["output"] = f"{results_dir}/{out_dir.split('/')[-1]}/{basename}"
+        with open(f"{out_dir}/{basename}", "w") as f:
+            json.dump(file_data, f)
+
+
+def get_files(base_dir):
+    files_path = list()
+    for file in os.listdir(base_dir):
+        path = os.path.join(base_dir, file)
+        if os.path.isdir(path):
+            files_path += get_files(path)
+        else:
+            files_path.append(path)
+    return files_path
+
+
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+def run_test(bin_file, config):
+    subprocess.run([bin_file, '-c', config])
+
+
+def get_results_as_csv(results_dir, save=None):
+    results = get_files(results_dir)
+    data = list()
+    for fpath in results:
+        loaded = load_json(fpath)
+        loaded.pop('tree', None)
+        data.append(loaded)
+    df = pd.DataFrame(data)
+    if save is not None:
+        df.to_csv(save, index=False)
+        print(f"File Saved in {save}")
+    return df
